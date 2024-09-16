657f8ccdeb5f0604eb8aab8473df3e9e781878e1
ninja3571 <125036855+ninja3571@users.noreply.github.com>
2024-09-17 01:16:03 +0200

    1.2

--- next1/src/app/components/image.js
+++ next1/src/app/components/image.js
@@ -3,10 +3,11 @@ import Image from "next/image";
 export default function Imagge() {
   return (
     <Image
-      src="/components/cat.png"
+      src="/cat.png"
       width={500}
       height={300}
       alt="image"
+      priority
     />
   )
 }

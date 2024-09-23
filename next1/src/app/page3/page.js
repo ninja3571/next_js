1e0a1df19dd8bec90ecfff0b835cc90d04f54d77
ninja3571 <125036855+ninja3571@users.noreply.github.com>
2024-09-23 16:56:02 +0200

    1.4

--- next1/src/app/page3/page.js
+++ next1/src/app/page3/page.js
@@ -6,6 +6,7 @@ export default function Home() {
     return (
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <Pages></Pages>
+        <h1 style={{color:'orange'}}>Page 3</h1>
         <ConditionalDisplay
         isVisible='true'/>
         <SimpleForm
